cbc356bd6b49638396de45603b86553ade080a4d
Rodrigo Poblete <pobletedev@gmail.com>
2022-11-10 15:46:43 -0300

    nuevos endpoints

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import { createPool } from "mysql2/promise";
 import { HOST, USER, PASSWORD, DB } from "./config.js";
 
 export const pool = createPool ({
-    host: HOST,
-    user: USER,
-    password: PASSWORD,
-    database: DB,
+    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+    user: 'bsale_test',
+    password: 'bsale_test',
+    database: 'bsale_test',
     connectTimeout: 1000*600,
 });
