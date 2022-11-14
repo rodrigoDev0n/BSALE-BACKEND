6371d4dd5a9dc707a1053255d8c3e0078d95badb
Rodrigo Poblete <pobletedev@gmail.com>
2022-11-13 23:17:06 -0300

    quitando puerto

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const HOST = process.env.DB_HOST || 'mdb-test.c6vunyturrl6.us-west-1.rds.
 export const USER = process.env.DB_USER || 'bsale_test';
 export const PASSWORD = process.env.DB_PASSWORD || 'bsale_test';
 export const DB = process.env.DB_DATABASE || 'bsale_test';
-export const PORT = process.env.DB_PORT || 3306;
+export const PORT = process.env.DB_PORT
