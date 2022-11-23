54f632c540fb34e556350e4c354f44f4a49f8e78
Rodrigo Poblete <pobletedev@gmail.com>
2022-11-22 21:00:13 -0300

    quitando credenciales

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from 'dotenv';
 
 config();
 
-export const HOST = process.env.DB_HOST || 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com';
-export const USER = process.env.DB_USER || 'bsale_test';
-export const PASSWORD = process.env.DB_PASSWORD || 'bsale_test';
-export const DB = process.env.DB_DATABASE || 'bsale_test';
-export const PORT = process.env.DB_PORT
+export const HOST = process.env.DB_HOST;
+export const USER = process.env.DB_USER;
+export const PASSWORD = process.env.DB_PASSWORD;
+export const DB = process.env.DB_DATABASE ;
+export const PORT = process.env.DB_PORT;
